3af0f9845251b2686b0b3470d96431b22c93db58
Subhra <shubhved0429@gmail.com>
2026-05-21 10:24:31 +0530

    Added PWA support

--- app.js
+++ app.js
@@ -313,3 +313,25 @@ document
     }
 
 });
+if ("serviceWorker" in navigator) {
+
+  window.addEventListener("load", () => {
+
+    navigator.serviceWorker
+      .register("/service-worker.js")
+
+      .then(() => {
+
+        console.log("Service Worker Registered");
+
+      })
+
+      .catch((error) => {
+
+        console.log(error);
+
+      });
+
+  });
+
+}
